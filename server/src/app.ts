f92c12953628b5e2cf117315ef77a275c7c0aca7
Minh Le <qminh279@gmail.com>
2024-02-06 01:20:05 -0500

    connect to mongodb and add packages

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,32 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
+import "express-async-errors";
+import config from "./utils/config";
 
 const app = express();
+
+// MONGODB connection
+mongoose.set("strictQuery", false);
+console.log("connecting to", config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI as string)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error: unknown) => {
+    let errorMessage = "Something went wrong: ";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.error("error connecting to MongoDB:", errorMessage);
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
 
 export default app;

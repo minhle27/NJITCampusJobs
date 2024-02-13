29e92d52e64b8234a66c2f598e2801c03972635e
Minh Le <qminh279@gmail.com>
2024-02-13 13:39:17 -0500

    add types for user and employer

---
+++ server/src/types.ts
@@ -0,0 +1,25 @@
+interface User {
+  password: string;
+  email: string;
+  fullname: string;
+  phone: string;
+  profileDescription?: string,
+  profilePicture?: string,
+}
+
+export interface Student extends User {
+  accountType: "student",
+  classYear: {
+    start: number,
+    end: number,
+  },
+  degree: string,
+  resume?: string,
+  transcript?: string,
+  major: string,
+}
+
+export interface Employer extends User {
+  accountType: "employer",
+  department: string,
+}

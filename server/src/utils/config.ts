b92081c1298a354f48efb3875bb19ff517e4851e
Minh Le <qminh279@gmail.com>
2024-02-12 21:23:15 -0500

    configure cloudinary

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,10 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
-require("dotenv").config();
+import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export default {
   MONGODB_URI,
   PORT,
+  cloudinary: cloudinary.v2,
 };

dd18ba6a4164d098c939778e86746c2a5fca406a
zoronguyen <mr.thai28022005@gmail.com>
2024-02-14 02:19:42 -0500

    add job and employer seeding

---
+++ server/src/seeding/employerSeeding.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import EmployerModel from "../models/Employer";
+const faker = require("faker");
+//import config from "../utils/config";
+
+async function seedEmployers() {
+  try {
+    // Connect to MongoDB
+    //await mongoose.connect(config.TEST_MONGODB_URI as string);
+    await mongoose.connect(
+      "mongodb+srv://qminh279:leminh279@cluster1.67dtx5d.mongodb.net/testNcjApp?retryWrites=true&w=majority"
+    );
+    console.log("Connected to Test MongoDB");
+
+    const seedCounts = 1;
+    const employers = [];
+
+    for (let i = 0; i < seedCounts; i++) {
+
+      const employer = {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        fullName: faker.name.findName(),
+        phone: faker.phone.phoneNumberFormat().replace(/-/g, ""),
+        department: faker.lorem.words(10),
+        profileDescription: faker.lorem.sentence(),
+        accountType: "employer",
+      };
+
+      employers.push(employer);
+    }
+
+    await EmployerModel.insertMany(employers);
+    console.log("Employers seeded successfully!");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // Close MongoDB connection
+    mongoose.disconnect();
+  }
+}
+
+seedEmployers();

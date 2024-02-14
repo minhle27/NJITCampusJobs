e9abacb78725b0ce5ad82331ca2ec2dcf709d667
Minh Le <qminh279@gmail.com>
2024-02-13 22:21:00 -0500

    set up endpoints and add some middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,9 @@ import "express-async-errors";
 import config from "./utils/config";
 
 import unknownEndpoint from "./middleware/unknownEndpoints";
+import errorHandler from "./middleware/errorHandler";
+
+import authRouter from "./routes/auth";
 
 const app = express();
 
@@ -26,11 +29,29 @@ mongoose
     console.error("error connecting to MongoDB:", errorMessage);
   });
 
-app.use(express.json());
+// Middleware
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
 
+// Routes
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.use("/api/auth", authRouter);
+
+// Middleware
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 export default app;

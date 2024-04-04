accbb72d3365b644b75526598707d9d39b9ab0e8
Minh Le <qminh279@gmail.com>
2024-04-04 15:47:27 -0400

    fix navbar

--- react-ui/src/Components/Modules/NavBar.tsx
+++ react-ui/src/Components/Modules/NavBar.tsx
@@ -4,8 +4,9 @@ import Logo from "../../assets/NCJ-logos_transparent.svg";
 import ProfileImage from "../../assets/Sample-Profile.svg";
 import NotificationImage from "../../assets/NotificationImage.svg";
 import { useAppDispatch } from "../../app/hooks";
-import { setCredentials } from "../../state/authSlice.ts"
+import { setCredentials } from "../../state/authSlice.ts";
 import { useGetAccountType } from "../../hooks/useGetAccounType.ts";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 // import { useGetEmployerQuery } from "../../services/apiSlice.ts";
 
 const NavBar = () => {
@@ -26,38 +27,51 @@ const NavBar = () => {
     <nav className="mx-auto flex w-full items-center justify-between shadow-md border-black">
       <div className="flex items-center">
         <img src={Logo} className="ml-2" alt="Logo" />
-        {["/Jobs", "/Profile", "/Calendar", "/Inbox", accountType === "employer" ? "/Dashboard" : "/Application"].map(
-          (route, id) => (
-            <button
-              key={id}
-              className="customNavLink"
-              onClick={() => {
-                if (route === "/Jobs") {
-                  return handleClick("/");
-                }
-                if (route === "/Dashboard") {
-                  return handleClick(`/dashboard/${auth.user!.id}`);
-                }
-                return handleClick(route.substring(1).toLowerCase());
-              }}
-            >
-              {route.substring(1)}
-            </button>
-          )
-        )}
+        {[
+          "/Jobs",
+          "/Profile",
+          "/Calendar",
+          "/Inbox",
+          accountType === "employer" ? "/Dashboard" : "/Application",
+        ].map((route, id) => (
+          <button
+            key={id}
+            className="customNavLink"
+            onClick={() => {
+              if (route === "/Jobs") {
+                return handleClick("/");
+              }
+              if (route === "/Dashboard") {
+                return handleClick(`/dashboard/${auth.user!.id}`);
+              }
+              return handleClick(route.substring(1).toLowerCase());
+            }}
+          >
+            {route.substring(1)}
+          </button>
+        ))}
       </div>
       <div className="flex justify-end mr-6 relative">
         <div className="flex">
-          <button onClick={handleLogout} className="customNavLink">
-            Logout
-          </button>
           <img src={NotificationImage} className="mr-4" />
           {/* <div
             role="alert"
             className="absolute top-0 right-0 z-10 bg-red-600 rounded-full p-1 text-xs mr-12"
           ></div> */}
         </div>
-        <img src={ProfileImage} className="my-2" />
+        <Menu>
+          <MenuButton
+            as={Button}
+            variant="link"
+            p={0}
+            _hover={{ opacity: 0.75 }}
+          >
+            <img src={ProfileImage} className="my-2" />
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={handleLogout}>Log Out</MenuItem>
+          </MenuList>
+        </Menu>
       </div>
     </nav>
   );

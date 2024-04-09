0e6a0de6dc4c1f3647138aab98690d1762b1e03e
Minh Le <qminh279@gmail.com>
2024-04-09 16:53:29 -0400

    redesign models and update seeding

---
+++ server/src/models/Application.ts
@@ -0,0 +1,39 @@
+import mongoose, { InferSchemaType } from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+    resumeUrl: {
+      type: String,
+    },
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+    },
+    status: String,
+  },
+  { timestamps: true }
+);
+
+type employerSchemaInferType = InferSchemaType<typeof applicationSchema>;
+
+applicationSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    if ("_id" in returnedObject) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+      returnedObject.id = returnedObject._id.toString();
+      delete returnedObject._id;
+      delete returnedObject.__v;
+    }
+  },
+});
+
+const applicationModel = mongoose.model<employerSchemaInferType>(
+  "Application",
+  applicationSchema
+);
+
+export default applicationModel;
--- server/src/seeding/seed.ts
+++ server/src/seeding/seed.ts
@@ -1,6 +1,7 @@
 import seedEmployer from "./employerSeeding";
 import seedJobs from "./JobSeeding";
 import seedStudents from "./studentSeeding";
+import seedApplications from "./applicationSeeding";
 
 console.log("Seeding data to test database...");
 
@@ -8,6 +9,7 @@ const seed = async () => {
   await seedEmployer();
   await seedStudents();
   await seedJobs();
+  await seedApplications();
 };
 
 void seed();

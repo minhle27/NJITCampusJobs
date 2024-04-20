582ecba578b9715156f8b1443955b05478b5081f
Kairy Vu <kairyvd167@gmail.com>
2024-04-20 10:06:27 -0400

    update jobdetail

--- react-ui/src/Components/Pages/JobDetail/JobDetail.tsx
+++ react-ui/src/Components/Pages/JobDetail/JobDetail.tsx
@@ -1,13 +1,15 @@
 import PostExcerpt from "../EmployerDashboard/PostExcerpt";
 import { useGetAllPostsQuery } from "../../../services/apiSlice";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useMemo } from "react";
 import { createSelector } from "@reduxjs/toolkit";
 import { JobPost } from "../../../types";
+import JobDetailChild from "../../Modules/JobDetailChild";
+import { Spinner } from "@chakra-ui/react";
 
-const JobDetailOptions = () => {
+const JobDetail = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const selectPostByID = useMemo(() => {
     return createSelector(
@@ -24,11 +26,22 @@ const JobDetailOptions = () => {
     }),
   });
 
-  return <></>;
-};
-
-const JobDetail = ({ post }) => {
-  return <></>;
+  if (!postById) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner />
+      </div>
+    );
+  }
+  return (
+    <section className="flex flex-col h-full overflow-auto">
+      <section className="flex flex-col justify-center items-center grow">
+        <PostExcerpt post={postById!}>
+          <JobDetailChild post={postById!} />
+        </PostExcerpt>
+      </section>
+    </section>
+  );
 };
 
 export default JobDetail;
